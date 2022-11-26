1cfdeb1821d4796ce599ede7bde65b32f634e5b5
William Huang <hh921088586@berkeley.edu>
2022-11-25 18:32:42 -0800

    make it better and better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3006;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
